44636656bfb230397cfe69a8d745c71e5a1a164a
Witold Andreasik <witold.andreasik@gmail.com>
2020-04-29 14:48:15 +0200

    add styled components, absolute imports

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Container = styled.div``;
+
+function Navigation() {
+  return <div>text</div>;
+}
+export default Navigation;
