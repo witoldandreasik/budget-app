c19e33884002a59154d0a18fcc45f3f21dc76069
Witold Andreasik <witold.andreasik@gmail.com>
2020-05-04 22:18:17 +0200

    add loading indicator

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -7,12 +7,12 @@ import theme from "utils/theme";
 
 import { useTranslation } from "react-i18next";
 
-import { Navigation, Wrapper } from "components";
+import { Navigation, Wrapper, LoadingIndicator } from "components";
 
 function App() {
   const { i18n } = useTranslation();
   return (
-    <ThemeProvider theme={theme}>
+    <Fragment>
       <GlobalStyles />
 
       <Router>
@@ -40,15 +40,17 @@ function App() {
           </Switch>
         </Wrapper>
       </Router>
-    </ThemeProvider>
+    </Fragment>
   );
 }
 
 function RootApp() {
   return (
-    <React.Suspense fallback="Loading...">
-      <App />
-    </React.Suspense>
+    <ThemeProvider theme={theme}>
+      <React.Suspense fallback={<LoadingIndicator />}>
+        <App />
+      </React.Suspense>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const around = keyframes`  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }`;
+
+const Root = styled.div`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+  &:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid ${({ theme }) => theme.colors.gray.normal};
+    border-color: ${({ theme }) => theme.colors.gray.normal} transparent
+      ${({ theme }) => theme.colors.gray.normal} transparent;
+    animation: ${around} 1.2s linear infinite;
+  }
+`;
+
+function LoadingIndicator() {
+  return <Root></Root>;
+}
+export default LoadingIndicator;
