6ece7831f2b9602af819bce6d6f89b062b3b5059
Witold Andreasik <witold.andreasik@gmail.com>
2020-05-07 18:08:40 +0200

    add react redux libs, budget reducer

---
+++ src/data/constants/index.js
@@ -0,0 +1,13 @@
+export * from "./budget.constants";
+
+// LOADING STATES
+export const LOADING_STATES = {
+  INITIAL: "INITIAL",
+  LOADING: "LOADING",
+  LOADED: "LOADED",
+  FAILED: "FAILED",
+};
+
+export default {
+  LOADING_STATES,
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+
 import "./index.css.js";
 import "./i18n/i18n";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import configureStore from "data/store";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
