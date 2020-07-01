0a9eb3adcbd0dc632734afd5d11d30fab091e3a5
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-01 11:40:12 +0200

    Updates

---
+++ precache-manifest.6f204c2d0c64b21bc40d7995e07e811e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dccf75eee714698248bcaa2476a9dac",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "c33d7e5533caeb763f02",
+    "url": "/budget-app/static/css/3.14f3cd7d.chunk.css"
+  },
+  {
+    "revision": "0c01e42697699dcc8616",
+    "url": "/budget-app/static/js/0.094b7b42.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/budget-app/static/js/0.094b7b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33d7e5533caeb763f02",
+    "url": "/budget-app/static/js/3.56aa3d46.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/budget-app/static/js/3.56aa3d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f49bd38b1b1552c6f8e",
+    "url": "/budget-app/static/js/4.2eaaf6a7.chunk.js"
+  },
+  {
+    "revision": "7d114cda6b35eaad7ca7",
+    "url": "/budget-app/static/js/5.6abe2b60.chunk.js"
+  },
+  {
+    "revision": "829579252822f7dc7dd2",
+    "url": "/budget-app/static/js/6.fdd9e93f.chunk.js"
+  },
+  {
+    "revision": "744290343ef1a56acc9c",
+    "url": "/budget-app/static/js/7.1d6d5522.chunk.js"
+  },
+  {
+    "revision": "46cad0da932aedf4ff5a",
+    "url": "/budget-app/static/js/main.b56fd217.chunk.js"
+  },
+  {
+    "revision": "1db2a59201705b78dd2f",
+    "url": "/budget-app/static/js/runtime-main.c06d2454.js"
+  }
+]);
